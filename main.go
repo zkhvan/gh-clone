6930c74d8b9898443e4c482c81e989ef3ce26b90
Zhenya Khvan <zhenya.khvan@gmail.com>
2023-08-20 10:37:39 -0400

    chore: add boilerplate cobra command

--- main.go
+++ main.go
@@ -1,4 +1,22 @@
 package main
 
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
 func main() {
+	rootCmd := &cobra.Command{
+		Use:           "clone",
+		Short:         "Clone repositories.",
+		SilenceUsage:  true,
+		SilenceErrors: true,
+	}
+
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
